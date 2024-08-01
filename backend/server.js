65a2368ca7e531b52a710fab488ee6b338e0a75b
Divy Dodeja <divy.dodeja@emotorad.com>
2024-08-01 20:24:23 +0530

    corrected the deployment code

--- backend/server.js
+++ backend/server.js
@@ -28,10 +28,10 @@ app.use("/api/v1/form", formRoutes);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
